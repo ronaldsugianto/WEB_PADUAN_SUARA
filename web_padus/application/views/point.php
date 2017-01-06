c69b44f080008af346217011f8df7f7a109d8b70
ronaldsugianto <ronaldsugianto_15@kharisma.ac.id>
2017-01-06 21:47:28 +0800

    homepage, loginadmin, point

---
+++ web_padus/application/views/point.php
@@ -0,0 +1,11 @@
+               <a href="#point" class="page-scroll btn btn-xl">Next Point</a>
+            </div>
+        </div>
+    </header>
+    <!-- Header======================================= -->
+
+
+    <section id="point">
+    	
+<br><br>
+    </section>
