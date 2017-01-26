4f7fd1441cc6ddda5eef4d7dcae9e671d55ca73f
ronaldsugianto <ronaldsugianto_15@kharisma.ac.id>
2017-01-26 10:08:39 +0800

    perubahan

--- web_padus/application/config/config.php
+++ web_padus/application/config/config.php
@@ -25,6 +25,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $config['base_url'] = 'http://localhost/WEB_PADUAN_SUARA/web_padus';
 
+
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Index File
