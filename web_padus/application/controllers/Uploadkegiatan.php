d59498c619e2fbb241d27df18f019ca44dd8ae9c
ronaldsugianto <ronaldsugianto_15@kharisma.ac.id>
2017-02-13 13:48:23 +0800

    r

--- web_padus/application/controllers/Uploadkegiatan.php
+++ web_padus/application/controllers/Uploadkegiatan.php
@@ -6,8 +6,6 @@ class Uploadkegiatan extends CI_Controller {
 
 	public function index()
 	{
-				if(!isset($_SESSION['login'])) redirect(base_url().'loginadmin');
-
 		$this->load->view('templates/header');	
 		$this->load->view('admintemplates/uploadkegiatan');
 		$this->load->view('templates/footer');			
